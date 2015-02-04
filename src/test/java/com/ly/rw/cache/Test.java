e8bea61affc8dbd8a65f7716e0d1f29b13002bd3
zongshoujin <zsj00877@ly.com>
2015-02-04 11:20:24 +0800

    test

---
+++ src/test/java/com/ly/rw/cache/Test.java
@@ -0,0 +1,18 @@
+
+package com.ly.rw.cache;
+
+/**
+ * Description Of The Class<br/>
+ * QQ:1211956756 
+ *
+ * @author 	(宗寿金 07814)
+ * @version 1.0.0, 2015年2月4日-上午11:19:54
+ * @since 2015年2月4日-上午11:19:54
+ */
+public class Test {
+
+	public void test(){
+		System.out.println("aasasasas");
+	}
+}
+
